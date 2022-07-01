101fe9068051f55b73df8c9671e6855b82ad5be1
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-07-01 08:08:26 +0200

    🔥 give french name to verbose_names of fields in model

--- ArcheryBuddy/materials/models/arrows.py
+++ ArcheryBuddy/materials/models/arrows.py
@@ -29,34 +29,34 @@ class Arrow(models.Model):
 
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     # encoche
-    nock = models.CharField(max_length=60)
+    nock = models.CharField("encoche", max_length=60)
 
     # empennage
     
-    feathering_type = models.CharField(
+    feathering_type = models.CharField("type d'empennage",
         max_length=10,
         choices=FEATHERING_TYPE_CHOICES,
         default=FEATHERING_TYPE_CHOICES[0]
         )
-    feathering_brand = models.CharField(max_length=60)
-    feathering_color = models.CharField(max_length=60)
-    feathering_cock_color = models.CharField(max_length=60)
-    feathering_size = models.CharField(max_length=60)
-    feathering_angle = models.IntegerField() # in degrees
-    feathering_nock_distance = models.IntegerField() # in mm
+    feathering_brand = models.CharField("marque de plumes", max_length=60)
+    feathering_color = models.CharField("couleur d'empennage", max_length=60)
+    feathering_cock_color = models.CharField("couleur de plume coq", max_length=60)
+    feathering_size = models.CharField("taille", max_length=60)
+    feathering_angle = models.IntegerField("angle") # in degrees
+    feathering_nock_distance = models.IntegerField("distance à l'encoche") # in mm
     # pointe
-    tip_brand = models.CharField(max_length=60)
-    tip_profile = models.CharField(max_length=60)
-    tip_weight = models.CharField(max_length=60) # in grains
+    tip_brand = models.CharField("marque", max_length=60)
+    tip_profile = models.CharField("profil", max_length=60)
+    tip_weight = models.CharField("masse (grains)", max_length=60) # in grains
     # tube
-    tube_brand = models.CharField(max_length=60)
-    tube_material = models.CharField(
+    tube_brand = models.CharField("marque", max_length=60)
+    tube_material = models.CharField("matériau",
         max_length=10,
         choices=TUBE_MATERIAL_CHOICES,
         default=TUBE_MATERIAL_CHOICES[0]
         )
-    tube_length = models.FloatField()
-    tube_spine = models.FloatField()
-    tube_diameter = models.FloatField()
+    tube_length = models.FloatField("longueur")
+    tube_spine = models.FloatField("flèche/spine")
+    tube_diameter = models.FloatField("diametre exterieur")
     # general attributes
-    not_broken = models.BooleanField(default=True)
+    not_broken = models.BooleanField("en état d'utilisation", default=True)
