90f1e1b7a362ca78c24269ed3dadfed4e94001c6
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-08-04 03:35:21 +0200

    install django-debug-toolbar

--- ArcheryBuddy/DjangoConf/urls.py
+++ ArcheryBuddy/DjangoConf/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("accounts.urls")),
     path("arrows/", include("equipment.urls.arrows")),
+    path("practice/", include("records.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
