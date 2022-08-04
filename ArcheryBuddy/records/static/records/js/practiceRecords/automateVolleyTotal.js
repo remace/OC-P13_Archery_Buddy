605f5c78b40800f57188f8e9a554b3e85a222635
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-08-04 19:09:49 +0200

    ✨ automatic calculus on score sheet for totals

--- ArcheryBuddy/records/static/records/js/practiceRecords/automateVolleyTotal.js
+++ ArcheryBuddy/records/static/records/js/practiceRecords/automateVolleyTotal.js
@@ -1,5 +1,37 @@
-console.log('coucou')
-/*
-const fields = document.querySelectorAll(".text-fields")
-console.log(fields)
-*/
+const fields = document.querySelectorAll(".text-field")
+const totalFields = document.querySelectorAll('[id^="volley-total-"]')
+const totalField = document.querySelector('#total')
+
+fields.forEach((field) => {
+    field.addEventListener('change', () => {
+        updateVolleyTotal(field)
+        updateTotal()
+    })
+})
+
+
+function updateVolleyTotal(field) {
+    // get all the fields from the line and the total
+    volley = field.id.split("-")[0]
+    const volleyTotalField = document.querySelector(`#volley-total-${volley}`)
+    const sameVolleyFieldsSelector = `[id^="${volley}"]`
+    const sameVolleyFields = document.querySelectorAll(sameVolleyFieldsSelector)
+
+    // calculate the total
+    let total = 0
+    sameVolleyFields.forEach((field) => {
+        let value = parseInt(field.value)
+        total += (isNaN(value) ? 0 : value)
+    })
+    volleyTotalField.innerHTML = total
+}
+
+function updateTotal() {
+    let total = 0
+    totalFields.forEach((field) => {
+        let value = parseInt(field.innerHTML)
+        total += (isNaN(value) ? 0 : value)
+    })
+
+    totalField.innerHTML = `total de l'entrainement: ${total}`
+}
