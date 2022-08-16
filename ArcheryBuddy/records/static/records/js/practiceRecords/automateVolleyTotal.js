e798ac3a9e5b515e2447af411415bac81a2e0f5d
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-08-16 08:29:21 +0200

    ✨ colors in practice record fields

--- ArcheryBuddy/records/static/records/js/practiceRecords/automateVolleyTotal.js
+++ ArcheryBuddy/records/static/records/js/practiceRecords/automateVolleyTotal.js
@@ -4,6 +4,7 @@ const totalField = document.querySelector('#total')
 
 fields.forEach((field) => {
     field.addEventListener('change', () => {
+        update_color(field)
         updateVolleyTotal(field)
         updateTotal()
     })
@@ -34,4 +35,48 @@ function updateTotal() {
     })
 
     totalField.innerHTML = `total de l'entrainement: ${total}`
+}
+
+function update_color(field) {
+    const YELLOW_ZONE_COLORS = ['rgb(255, 255, 0)', 'rgb(0, 0, 0)']
+    const RED_ZONE_COLORS = ['rgb(255, 0, 0)', 'rgb(0, 0, 0)']
+    const BLUE_ZONE_COLORS = ['rgb(51, 167, 255)', 'rgb(0, 0, 0)']
+    const BLACK_ZONE_COLORS = ['rgb(0, 0, 0)', 'rgb(255, 255, 255)']
+    const WHITE_ZONE_COLORS = ['rgb(255, 255, 255)', 'rgb(0, 0, 0)']
+
+    score = parseInt(field.value)
+    console.log(score)
+
+    switch (score) {
+        case 10:
+        case 9:
+            console.log("couleur jaune")
+            field.style.background = YELLOW_ZONE_COLORS[0]
+            field.style.color = YELLOW_ZONE_COLORS[1]
+            break;
+        case 8:
+        case 7:
+            console.log("couleur rouge")
+            field.style.background = RED_ZONE_COLORS[0]
+            field.style.color = RED_ZONE_COLORS[1]
+            break;
+        case 6:
+        case 5:
+            console.log("couleur bleue")
+            field.style.background = BLUE_ZONE_COLORS[0]
+            field.style.color = BLUE_ZONE_COLORS[1]
+            break;
+        case 4:
+        case 3:
+            console.log("couleur noire")
+            field.style.background = BLACK_ZONE_COLORS[0]
+            field.style.color = BLACK_ZONE_COLORS[1]
+            break;
+        default:
+            console.log("couleur blanche")
+            field.style.background = WHITE_ZONE_COLORS[0]
+            field.style.color = WHITE_ZONE_COLORS[1]
+            break;
+    }
+
 }
