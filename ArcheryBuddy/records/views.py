27d4aeafe1b96295c6b3509c60f01a997c1d311d
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-09-15 05:28:16 +0200

    🚧 add get method for practice detail view

--- ArcheryBuddy/records/views.py
+++ ArcheryBuddy/records/views.py
@@ -1,10 +1,13 @@
+import re
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic.edit import FormView
 
-from records.models import PracticeRecordSession
+from equipment.models.arrows import Arrow
+from records.models import PracticeRecordSession, PracticeRecord
 from records.forms import PracticeRecordSessionForm
 
 
@@ -53,13 +56,60 @@ class DetailPracticeSession(View):
         ctx["prs"] = prs
         ctx["volley_range"] = range(1, prs.number_of_volleys + 1)
         ctx["arrow_range"] = range(1, prs.max_arrows_in_volley + 1)
+        practice_records = PracticeRecord.objects.filter(practice_session=prs).all()
+        shots = {}
+        for practice_record in practice_records:
+            shot = {}
+            shot["arrow_id"] = practice_record.arrow.id
+            shot["score"] = practice_record.score
+            try:
+                temp = shots[practice_record.volley]
+            except KeyError as key_error:
+                print(f"KeyError: {key_error}")
+                shots[practice_record.volley] = []
+            finally:
+                shots[practice_record.volley].append(shot)
+
+        ctx["practice_records"] = shots
         return render(request, "records/detail_practice_session.html", context=ctx)
 
     @method_decorator(login_required)
-    def post(self, request):
-        ctx = {}
-        form = PracticeRecordSessionForm(request.POST)
-        return render(request, "records/detail_practice_session.html", context=ctx)
+    def post(self, request, prs_id):
+
+        score_pattern = re.compile(r"input-score-[0-9]+-[0-9]+")
+        arrow_pattern = re.compile(r"input-arrow-[0-9]+-[0-9]+")
+        prs = PracticeRecordSession.objects.get(id=prs_id)
+        post = request.POST
+        for key, value in post.items():
+            if re.fullmatch(score_pattern, key):
+                if value != "":
+                    key_splitted = key.split("-")
+                    volley = key_splitted[2]
+                    shot = key_splitted[3]
+                    score = int(value)
+                    arrow_id = int(post.get(f"input-arrow-{volley}-{shot}"))
+
+                    try:
+                        arrow = Arrow.objects.get(id=arrow_id)
+                        try:
+                            practice_record = PracticeRecord.objects.filter(
+                                arrow=arrow,
+                                practice_session=prs,
+                                volley=volley,
+                            )
+
+                        except PracticeRecord.DoesNotExist as exception:
+                            print(f"DoesNotExist: {exception}")
+                            PracticeRecord.objects.create(
+                                arrow=arrow,
+                                practice_session=prs,
+                                volley=volley,
+                                score=score,
+                            )
+                    except Arrow.DoesNotExist as exception:
+                        print(f"{exception} arrow {arrow_id} does not exist")
+
+        return redirect("practice_detail", prs_id=prs.id)
 
 
 @login_required(login_url="/user/login/")
