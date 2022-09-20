5335f0e305789252215a7f7477140b33d13cad28
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-09-20 14:32:58 +0200

    🐛 corrected mistakes to settings modules

--- ArcheryBuddy/DjangoConf/settings/dev.py
+++ ArcheryBuddy/DjangoConf/settings/dev.py
@@ -21,8 +21,8 @@ def show_toolbar(request):
     return True
 
 
-INSTALLED_APPS += "debug_toolbar"
-MIDDLEWARE += "debug_toolbar.middleware.DebugToolbarMiddleware"
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": show_toolbar,
