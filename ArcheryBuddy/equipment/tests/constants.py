974896bb7d5a632a5743acb7c7ac93905d273f9a
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-11-29 08:08:49 +0100

    ✅ separated constants for testing in a separate file

--- ArcheryBuddy/equipment/tests/constants.py
+++ ArcheryBuddy/equipment/tests/constants.py
@@ -1,3 +1,66 @@
+BAREBOW = {
+    "bow_type": "Barebow",
+    "power": "38",
+    "laterality": "R",
+    "riser_brand": "sf forged",
+    "riser_size": "25",
+    "riser_color": "bleue",
+    "riser_grip": "noir",
+    "limbs_brand": "sf premium",
+    "limbs_power": "32",
+    "limbs_size": "68",
+    "string_brand": "Lyon Archerie",
+    "material": "8125",
+    "strands": "10",
+    "rest_type": "SM",
+    "rest_brand": "shibuya",
+    "berger_brand": "decut",
+    "berger_color": "bleu",
+    "berger_spring": "moyen",
+    "string_turns": "25",
+    "nockset_offset": "8",
+    "band": "14",
+    "high_tiller": "14",
+    "low_tiller": "14",
+}
+
+BAREBOW_NOT_VALID = BAREBOW.copy()
+BAREBOW_NOT_VALID.pop("power")
+
+OLYMPIC = {
+    "bow_type": "Olympic",
+    "power": "36",
+    "laterality": "R",
+    "riser_brand": "zmkfjzvnmn",
+    "riser_size": "25",
+    "riser_color": "bleu",
+    "riser_grip": "noir",
+    "limbs_brand": "mzenrm",
+    "limbs_power": "32",
+    "limbs_size": "68",
+    "string_brand": "lyon",
+    "material": "8125",
+    "strands": "10",
+    "rest_brand": "pzouvn",
+    "rest_type": "SM",
+    "berger_brand": "punn",
+    "berger_color": "lkmjfvn",
+    "berger_spring": "hbljbh",
+    "scope_brand": "lhbh",
+    "clicker_brand": "ljbhljb",
+    "stab_brand": "jhbjlhb",
+    "front_brand": "lbhbl",
+    "rears_brand": "ljbhjlh",
+    "string_turns": "25",
+    "nockset_offset": "8",
+    "band": "14",
+    "high_tiller": "14",
+    "low_tiller": "14",
+}
+
+OLYMPIC_NOT_VALID = OLYMPIC.copy()
+OLYMPIC_NOT_VALID.pop("scope_brand")
+
 COMPOUND = {
     "bow_type": "Compound",
     "power": "50",
@@ -19,3 +82,18 @@ COMPOUND = {
 
 COMPOUND_NOT_VALID = COMPOUND.copy()
 COMPOUND_NOT_VALID.pop("entraxe")
+
+
+if __name__ == "__main__":
+    import pprint
+
+    bows = {
+        "barebow": BAREBOW,
+        "barebow_not_valid": BAREBOW_NOT_VALID,
+        "olympic": OLYMPIC,
+        "olympic_not_valid": OLYMPIC_NOT_VALID,
+        "compound": COMPOUND,
+        "compound_not_valid": COMPOUND_NOT_VALID,
+    }
+
+    pprint.pprint(bows)
