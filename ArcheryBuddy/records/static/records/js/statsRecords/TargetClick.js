f2b2c73c93b9cfb26ad958ea6832a1125c31a115
Rémi TAUVEL <tauvel.remi@gmail.com>
2023-02-22 23:41:30 +0100

    🚑️ changed from my own modal (not working) to HTML5 Dialog (still not working)

--- ArcheryBuddy/records/static/records/js/statsRecords/TargetClick.js
+++ ArcheryBuddy/records/static/records/js/statsRecords/TargetClick.js
@@ -1,60 +1,41 @@
 const TARGET = document.querySelector("#target")
 const SHOTSLIST = document.querySelector("#shots-to-save")
 
-const MODAL = document.querySelector("#arrow-modal")
-const MODAL_SUBMIT = document.querySelector("#modal-arrow-submit")
-const MODAL_CANCEL = document.querySelector('#modal-arrow-cancel')
-
-console.log(MODAL) // should return a div, returns null
-console.log(MODAL_SUBMIT) // should return a div returns null
-console.log(MODAL_CANCEL) // should return a div returns null
+let DIALOG = document.querySelector("#arrow-modal")
+let DIALOG_SUBMIT = document.querySelector("#modal-arrow-submit")
+let DIALOG_CANCEL = document.querySelector("#modal-arrow-cancel")
+let DIALOG_SELECT = document.querySelector("#arrow-modal select")
 
 let recordCanceled = true
 let arrow_id = 0
 
-
-TARGET.addEventListener('click', (e) => {
-    let x = e.offsetX
-    let y = e.offsetY
-
-    console.log("target event")
-
-    //modal
-    MODAL.classList.toggle("hidden")
-    MODAL_SUBMIT.addEventListener('click', function modal_submit(e) {
-        arrow_id = 6 ///numero à extraire
-        MODAL.classList.toggle("hidden")
-        recordCanceled = false
-
-        MODAL_SUBMIT.removeEventListener('click', modal_submit())
-
-    })
-
-    MODAL_SUBMIT.addEventListener('click', function modal_cancel(e) {
-        MODAL.classList.toggle("hidden")
-        recordCanceled = true
-    })
+let x;
+let y;
 
 
-    if (recordCanceled !== false) {
-        return
+TARGET.addEventListener('click', function onOpen(e) {
+    x = e.offsetX
+    y = e.offsetY
+
+    DIALOG.showModal()
+})
+
+DIALOG_SELECT.addEventListener('change', function onSelect() {
+    DIALOG_SUBMIT.value = DIALOG_SELECT.value
+})
+
+DIALOG.addEventListener('close', function onClose() {
+    if (DIALOG.returnValue !== "default") {
+        // addPoint(x, y)
+        console.log(DIALOG.returnValue)
+        // addRecordToList(x, y, DIALOG.returnValue)
     }
-    add_point(x, y)
-    addRecordToList(x, y, arrow_id)
 })
 
-MODAL_SUBMIT.addEventListener('click', (e) => {
-    arrow_id = 6 ///numero à extraire
-    MODAL.classList.toggle("hidden")
-    recordCanceled = false
-})
 
-MODAL_SUBMIT.addEventListener('click', (e) => {
-    MODAL.classList.toggle("hidden")
-    recordCanceled = true
-})
 
-function add_point(x, y) {
+
+function addPoint(x, y) {
     let newDiv = document.createElement('div')
     newDiv.classList.add("z-10")
     newDiv.classList.add("w-2")
@@ -62,7 +43,7 @@ function add_point(x, y) {
     newDiv.classList.add("absolute")
     newDiv.classList.add(`left-[${x}px]`)
     newDiv.classList.add(`top-[${y}px]`)
-    newDiv.classList.add(`bg-black`)
+    newDiv.classList.add(`bg-green`)
     newDiv.classList.add(`rounded-full`)
     newDiv.style.top = `${y}px`
     newDiv.style.left = `${x}px`
